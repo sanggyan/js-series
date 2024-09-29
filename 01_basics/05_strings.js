dcdf70582fc232f32954945f00ed220ebcaeb3ea
Sangyan Bhandari <151984085+sanggyan@users.noreply.github.com>
2024-09-29 16:51:12 +0000

    string

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,10 @@
+const name = "sangyan";
+const repoCount = 50;
+
+//console.log(name + repoCount + "value"); this is outdated
+
+console.log(` Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String("sangyan");
+console.log(gameName[0]);
+console.log(typeof gameName);
