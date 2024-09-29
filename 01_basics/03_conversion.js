0c4bd2ef03eb36f404355d3587466004effae040
Sangyan Bhandari <151984085+sanggyan@users.noreply.github.com>
2024-09-29 07:48:05 +0000

    conversion

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,38 @@
+/*let score = false;
+
+console.log(typeof score);
+
+let valueInNumber = Number(score);
+
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+//"33" => 33
+// "33aabc"=> NaN
+// true => 1; false
+
+let num = "sangyan";
+
+let newNum = Boolean(num);
+console.log(newNum);
+
+
+let someNum= 10;
+let newNum = String(someNum);
+console.log(someNum);
+console.log(typeof newNum);
+*/
+
+//*****************operations*****************
+let num1 = 3;
+let num2 = "3"
+
+if(num1===num2){
+    console.log("yayyyy");
+}
+else{
+    console.log("nayyyy")
+}
+
+
+
