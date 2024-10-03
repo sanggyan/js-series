b936063f1865aee8fe5a875fcf9de6c7340f5361
Sangyan Bhandari <151984085+sanggyan@users.noreply.github.com>
2024-10-03 13:09:44 +0000

    strings

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -1,4 +1,4 @@
-const name = "sangyan";
+/*const name = "sangyan";
 const repoCount = 50;
 
 //console.log(name + repoCount + "value"); this is outdated
@@ -8,3 +8,18 @@ console.log(` Hello my name is ${name} and my repo count is ${repoCount}`);
 const gameName = new String("sangyan");
 console.log(gameName[0]);
 console.log(typeof gameName);
+
+const newGame = new String("red dead redemption");
+console.log(newGame[0]);
+console.log(newGame.length);
+console.log(newGame.toUpperCase);
+
+name = "sangyan";
+console.log(name.charAt(2));
+console.log(name.indexOf('n'));*/
+
+newName = " sanggyan   ";
+console.log(newName);
+console.log(newName.trim());
+console.log(newName[-1]);
+
