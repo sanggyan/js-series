eb989eaac752162f03979a5f6e451dc3df56ced1
Sangyan Bhandari <151984085+sanggyan@users.noreply.github.com>
2024-10-03 13:10:03 +0000

    numsandmath

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,17 @@
+/*score = 99;
+
+console.log(score);
+
+const newScore = new Number(100);
+console.log(newScore);
+
+console.log(score.toString());
+console.log(newScore.toString());
+console.log(typeof(score));
+console.log(typeof(newScore));*/
+
+newNum = 99.54739
+newNewNum = newNum.toFixed(2);//returns string
+
+console.log(typeof(newNewNum));
+
